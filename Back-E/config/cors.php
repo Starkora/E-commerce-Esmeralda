01d16a047182d602877494c4df4d7647e2f55dfd
Esteban Eduardo Cardenas Perez <esteban.cardenas@inetum.com>
2025-10-14 22:21:36 -0500

    Permite CORS desde dominio de frontend en Vercel

--- Back-E/config/cors.php
+++ Back-E/config/cors.php
@@ -8,6 +8,7 @@ return [
     'allowed_origins' => [
         'http://localhost:3000',
         'http://127.0.0.1:3000',
+        'https://e-commerce-esmeralda.vercel.app',
     ],
     'allowed_headers' => ['*'],
     'supports_credentials' => true,
